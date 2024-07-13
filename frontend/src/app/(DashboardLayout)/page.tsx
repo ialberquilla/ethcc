482a1a05f14cb9865644b58fc0931ddb469b0a23
Ivan <ivan.alberquilla@gmail.com>
2024-07-13 11:45:11 +0200

    integrate game

--- frontend/src/app/(DashboardLayout)/page.tsx
+++ frontend/src/app/(DashboardLayout)/page.tsx
@@ -15,7 +15,17 @@ const Dashboard = () => {
       <Box>
         <Grid container spacing={3}>
           <Grid item xs={12} lg={12}>
-            <ProfitExpenses />
+          <iframe
+              id={"1"}
+              src={`http://127.0.0.1:3003`}
+              width={1200}
+              height={801}
+              allow='fullscreen'
+              style={{
+                border: "none",
+                borderRadius: "0.5rem"
+              }}
+            />
           </Grid>
           <Grid item xs={12} lg={12}>
             <TopPayingClients />
