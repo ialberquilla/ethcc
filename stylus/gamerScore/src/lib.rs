83660596827cc459be18305a3a22b8694fe0fe09
Ivan <ivan.alberquilla@gmail.com>
2024-07-13 20:36:15 +0200

    uint convert

--- stylus/gamerScore/src/lib.rs
+++ stylus/gamerScore/src/lib.rs
@@ -12,6 +12,10 @@ use alloy_sol_types::sol;
 
 use stylus_sdk::prelude::*;
 
+use ethereum_transaction::types::U256;
+use num_bigint::BigEndian;
+use std::convert::TryInto;
+
 sol_storage! {
     #[entrypoint]
     pub struct Gamers {
@@ -44,37 +48,48 @@ impl Gamers {
         let score = self.gamer_entries_scores.get(gamer).get(index);
         let spent_time = self.gamer_entries_time_spent.get(gamer).get(index);
 
-        (
-            score,
-            spent_time
-        )
+        (score, spent_time)
     }
 
     pub fn add_gamer(&mut self, gamer: Address) {
         let mut total_gamers = self.total_gamers.get();
         self.gamers.insert(total_gamers, gamer);
-        self.total_gamers.set(total_gamers +  U256::from(1));
+        self.total_gamers.set(total_gamers + U256::from(1));
     }
 
     pub fn add_gamer_entry(&mut self, gamer: Address, score: U256, spent_time: U256) {
         let mut total_entries = self.total_gamer_entries.get(gamer);
-        self.gamer_entries_scores.setter(gamer).insert(total_entries, score);
-        self.gamer_entries_time_spent.setter(gamer).insert(total_entries, spent_time);
-        
+        self.gamer_entries_scores
+            .setter(gamer)
+            .insert(total_entries, score);
+        self.gamer_entries_time_spent
+            .setter(gamer)
+            .insert(total_entries, spent_time);
+
         let mut total_entries = self.total_gamer_entries.setter(gamer);
         let old_total_entries = total_entries.get();
         total_entries.set(old_total_entries + U256::from(1));
+
+        update_user_score(gamer);
     }
 
     pub fn update_user_score(&mut self, gamer: Address) {
         let mut total_entries = self.total_gamer_entries.get(gamer);
         let mut total_score = U256::from(0);
 
-        // for i in 0..total_entries {
-        //     let score = self.gamer_entries_scores.get(gamer).get(U256::from(i));
-        //     total_score += score;
-        // }
+        for i in 0..u256_to_i64(total_entries) {
+            let score = self.gamer_entries_scores.get(gamer).get(U256::from(i));
+            total_score += score;
+        }
 
-        // self.gamer_scores.set(gamer, total_score);
+        self.gamer_scores.set(gamer, total_score);
+    }
+
+    fn u256_to_i64(u256_value: U256) -> i64 {
+        let mut bytes = vec![0; 8];
+        u256_value.to_bytes(&mut bytes);
+
+        let result = i64::from_ne_bytes(bytes);
+        result
     }
 }
